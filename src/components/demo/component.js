72aeec8bbcbe7aa3c2f81feb0290c391ecf6b3fa
Abu Sayed (AEM Frontend & UI) <abu.sayed@abbvie.com>
2024-11-06 19:30:27 +0600

    Routing done with dynamic active route

---
+++ src/components/demo/component.js
@@ -0,0 +1,8 @@
+// import buttonLink from "../button-link/index.marko";
+
+export default class {
+
+    handleAddTabButtonClick() {
+        alert("Clicked")
+    }
+}
--- src/index.js
+++ src/index.js
@@ -1,7 +1,9 @@
 import express from "express";
 import compressionMiddleware from "compression";
 import markoMiddleware from "@marko/express";
-import indexPage from "./pages/index";
+import indexTemplate from "./pages/index/template.marko";
+import aboutTemplate from "./pages/about/template.marko";
+import contactTemplate from "./pages/contact/template.marko";
 import usersService from "./services/users";
 
 const port = parseInt(process.env.PORT || 3000, 10);
@@ -10,7 +12,25 @@ express()
   .use(compressionMiddleware()) // Enable gzip compression for all HTTP responses.
   .use("/assets", express.static("dist/assets")) // Serve assets generated from webpack.
   .use(markoMiddleware()) // Enables res.marko.
-  .get("/", indexPage)
+
+  // * Start Router configurations
+  .get("/", (req, res) => {
+    res.marko(indexTemplate, {
+      $global: { currentPath: req.path }
+    });
+  })
+  .get("/about-us", (req, res) => {
+    res.marko(aboutTemplate, {
+      $global: { currentPath: req.path }
+    });
+  })
+  .get("/contact-us", (req, res) => {
+    res.marko(contactTemplate, {
+      $global: { currentPath: req.path }
+    });
+  })
+  // # End router configurations
+
   .get("/services/users", usersService)
   .listen(port, err => {
     if (err) {
